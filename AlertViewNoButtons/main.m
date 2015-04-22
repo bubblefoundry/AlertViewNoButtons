5bf5685cf48cfdf178df066611f6eb79062ea2eb
Peter Robinett <peter@getlua.com>
2015-04-22 15:42:03 -0400

    Initial Commit

---
+++ AlertViewNoButtons/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AlertViewNoButtons
+//
+//  Created by Peter Robinett on 4/22/15.
+//  Copyright (c) 2015 Lua Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ AlertViewNoButtonsTests/AlertViewNoButtonsTests.m
@@ -0,0 +1,40 @@
+//
+//  AlertViewNoButtonsTests.m
+//  AlertViewNoButtonsTests
+//
+//  Created by Peter Robinett on 4/22/15.
+//  Copyright (c) 2015 Lua Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface AlertViewNoButtonsTests : XCTestCase
+
+@end
+
+@implementation AlertViewNoButtonsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
